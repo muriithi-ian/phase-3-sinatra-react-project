4917d15f6dc41bfb0778707ca07ad168d318e10e
Mildred <mildredkoskei01@gmail.com>
2022-11-08 15:15:02 +0300

    added all the crud options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,42 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/books" do
     books = Book.all
     books.to_json
   end
+
   get '/books/:id' do
     book = Book.find(params[:id])
-    book.to_json(include: :categories)
+    book.to_json
   end
+
+  delete '/books/:id' do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
+  end
+
+  post '/books' do
+    book = Book.create(
+      title:params[:title],
+      author:params[:author], 
+      description:params[:description],
+      category_id:params[:category_id], 
+      image:params[:image], 
+      available:params[:available]
+    )
+    book.to_json
+  end
+
+  patch '/books/:id' do
+    book = Book.find(params[:id])
+    book.update(
+      available: params[:available]
+    )
+    book.to_json
+  end
+
+ 
 
 end
